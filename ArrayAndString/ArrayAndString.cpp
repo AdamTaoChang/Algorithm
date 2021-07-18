aee9e8641347f8da392d73dce6560b3c258b0aab
HuberyTao <HuberyTao@126.com>
2021-07-18 22:43:19 +0800

    有序数组插入位置

--- ArrayAndString/ArrayAndString.cpp
+++ ArrayAndString/ArrayAndString.cpp
@@ -2,9 +2,58 @@
 //
 
 #include <iostream>
+#include <vector>
+
+
+class Solution 
+{
+
+public:
+   
+    //双指针 双index 二分法的写法？
+    static  int searchInsert(std::vector<int>& nums, int target) 
+    {
+        int nLeftIndex = 0; 
+        int nRightIndex = nums.size() - 1;
+        bool bFindTarget = false;
+        while (nLeftIndex < nRightIndex)
+        {
+            int nMidIndex = (nLeftIndex + nRightIndex) / 2;
+            if (nums[nMidIndex] == target)
+            {
+                bFindTarget = true;
+                return nMidIndex;
+            }
+            else if (nums[nMidIndex] < target)
+            {
+                nLeftIndex = nMidIndex + 1;
+            }
+            else
+            { 
+                nRightIndex = nMidIndex - 1;
+            }
+        }
+        if (!bFindTarget)
+        {
+            if (target > nums[nLeftIndex])
+            {
+                return nLeftIndex + 1;
+            }
+        }
+
+        return nLeftIndex;
+    }
+};
+
 
 int main()
 {
+    std::vector<int> vecNums;//[1, 3, 5, 6]
+    vecNums.push_back(1);
+    vecNums.push_back(3);
+    vecNums.push_back(5);
+    vecNums.push_back(6);
+    int index = Solution::searchInsert(vecNums, 2);
     std::cout << "Hello World!\n";
 }
 
--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -240,7 +240,6 @@ public:
 int main()
 {
 
-
     std::vector<int> preorder;
     //[3, 9, 20, 15, 7]
     //[9, 3, 15, 20, 7]
